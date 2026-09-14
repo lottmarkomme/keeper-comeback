58af5c97f551d12041138925ae91541e3c357f03
lottmarkomme <lottmarkomme21@gmail.com>
2026-09-14 08:08:41 +0200

    fix: scope PWA assets for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -20,13 +20,13 @@ export default defineConfig({
         start_url: './',
         scope: './',
         icons: [
-          { src: '/pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/pwa-512x512.png', sizes: '512x512', type: 'image/png' },
-          { src: '/pwa-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
+          { src: './pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: './pwa-512x512.png', sizes: '512x512', type: 'image/png' },
+          { src: './pwa-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
         ]
       },
       workbox: {
-        navigateFallback: '/index.html',
+        navigateFallback: 'index.html',
         globPatterns: ['**/*.{js,css,html,svg,png,woff2}']
       }
     })
